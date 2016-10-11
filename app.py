6babc7689a53a97514d79f74aad502ca1be27231
kkevinyang <sygtxyj@gmail.com>
2016-10-11 14:52:10 +0800

    add require and igore

--- app.py
+++ app.py
@@ -17,6 +17,18 @@ def make_shell_context():
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
+# 部署命令
+@manager.command
+def deploy():
+    """Run deployment tasks."""
+    from flask_migrate import upgrade
+    from app.models import Role, User
+
+    # migrate database to latest revision
+    upgrade()
+
+    # create user roles
+    Role.insert_roles()
 
 if __name__ == '__main__':
     manager.run()
